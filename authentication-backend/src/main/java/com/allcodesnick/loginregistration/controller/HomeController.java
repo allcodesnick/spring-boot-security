9723ef0a6134c4ff386553d45ecd791e4cbdc326
Nick <nfelix1997@gmail.com>
2021-12-07 20:49:53 -0500

    modified home controller

--- authentication-backend/src/main/java/com/allcodesnick/loginregistration/controller/HomeController.java
+++ authentication-backend/src/main/java/com/allcodesnick/loginregistration/controller/HomeController.java
@@ -2,12 +2,10 @@ package com.allcodesnick.loginregistration.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/")

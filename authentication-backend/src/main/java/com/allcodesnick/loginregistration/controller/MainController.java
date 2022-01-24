0cde690bc89370b387b8792667491c0731b899e7
Nick <nfelix1997@gmail.com>
2022-01-23 23:27:41 -0500

    created admin page table that displays user_id and usernames

--- authentication-backend/src/main/java/com/allcodesnick/loginregistration/controller/MainController.java
+++ authentication-backend/src/main/java/com/allcodesnick/loginregistration/controller/MainController.java
@@ -1,10 +1,18 @@
 package com.allcodesnick.loginregistration.controller;
 
+import com.allcodesnick.loginregistration.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    private UserService userService;
+
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/login")
     public String login() {
@@ -12,7 +20,8 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("UsersList", userService.listUserAccounts());
         return "index";
     }
 }
